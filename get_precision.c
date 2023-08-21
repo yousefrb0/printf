d682011fc7e0bd64edc494a74ad9f54ec8b9656f
mahmoud <mahlawyuao07@gmail.com>
2023-08-21 15:58:05 -0700

    L

--- get_precision.c
+++ get_precision.c
@@ -2,43 +2,43 @@
 
 /**
  * get_precision - Calculates the precision for printing
- * @format: Formatted string containing format specifiers.
- * @i: Index of the current character in the format string.
- * @list: List of arguments passed to _printf.
+ * @format: Formatted string in which to print the arguments
+ * @i: Index of the current character in the format string
+ * @list: List of arguments passed to _printf
  *
- * Return: Precision value.
+ * Return: Calculated precision value
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-    int curr_i = *i + 1;    // Initialize current index after '.'
-    int precision = -1;     // Initialize precision to -1 (default)
+	int curr_i = *i + 1; // Initialize current index after '.'
+	int precision = -1; // Initialize precision to -1 (default)
 
-    // Check if precision specifier exists
-    if (format[curr_i] != '.')
-        return (precision);
+	// Check if precision specifier exists
+	if (format[curr_i] != '.')
+		return (precision);
 
-    precision = 0; // Reset precision
+	precision = 0; // Reset precision
 
-    // Loop through characters after '.'
-    for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-    {
-        if (is_digit(format[curr_i])) // If current character is a digit
-        {
-            precision *= 10;
-            precision += format[curr_i] - '0'; // Calculate precision value
-        }
-        else if (format[curr_i] == '*') // If '*' character is encountered
-        {
-            curr_i++; // Move to the next character
-            precision = va_arg(list, int); // Get precision from variable argument list
-            break; // Exit loop after getting precision from '*'
-        }
-        else
-            break; // Exit loop if an invalid character is encountered
-    }
+	// Loop through characters after '.'
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i])) // If current character is a digit
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0'; // Calculate precision value
+		}
+		else if (format[curr_i] == '*') // If '*' character is encountered
+		{
+			curr_i++; // Move to the next character
+			precision = va_arg(list, int); // Get precision from variable argument list
+			break; // Exit loop after getting precision from '*'
+		}
+		else
+			break; // Exit loop if an invalid character is encountered
+	}
 
-    *i = curr_i - 1; // Update the index to the last processed character
+	*i = curr_i - 1; // Update the index to the last processed character
 
-    return (precision); // Return the calculated precision
+	return (precision); // Return the calculated precision
 }
 
--- get_width.c
+++ get_width.c
@@ -1,38 +1,37 @@
+
 #include "main.h"
 
 /**
  * get_width - Calculates the width for printing
- * @format: Formatted string containing format specifiers.
- * @i: Index of the current character in the format string.
- * @list: List of arguments passed to _printf.
+ * @format: Formatted string in which to print the arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
  *
- * Return: Width value.
+ * Return: width.
  */
 int get_width(const char *format, int *i, va_list list)
 {
-    int curr_i; // Initialize current index after current character
-    int width = 0; // Initialize width to 0
+	int curr_i;
+	int width = 0;
 
-    // Loop through characters after the current index
-    for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-    {
-        if (is_digit(format[curr_i])) // If current character is a digit
-        {
-            width *= 10; // Multiply the existing width by 10
-            width += format[curr_i] - '0'; // Add the digit value to the width
-        }
-        else if (format[curr_i] == '*') // If '*' character is encountered
-        {
-            curr_i++; // Move to the next character
-            width = va_arg(list, int); // Get width from variable argument list
-            break; // Exit loop after getting width from '*'
-        }
-        else
-            break; // Exit loop if an invalid character is encountered
-    }
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			width *= 10;
+			width += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
 
-    *i = curr_i - 1; // Update the index to the last processed character
+	*i = curr_i - 1;
 
-    return (width); // Return the calculated width
+	return (width);
 }
-
